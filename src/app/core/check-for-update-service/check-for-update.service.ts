3b53e7cebd3054ecd7a9728136aa1731028a094b
ronnymikalsen <ronny.mikalsen@nb.no>
2018-03-04 12:13:16 +0100

    fix: check if sw updates is enabled

--- src/app/core/check-for-update-service/check-for-update.service.ts
+++ src/app/core/check-for-update-service/check-for-update.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar, MatIconRegistry } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 import { interval } from 'rxjs/observable/interval';
 import { startWith } from 'rxjs/operators';
 
 @Injectable()
 export class CheckForUpdateService {
   constructor(updates: SwUpdate, private snackBar: MatSnackBar) {
-    if (updates) {
-      interval(6 * 60 * 60)
-        .pipe(startWith(0))
-        .subscribe(() => {
-          updates.checkForUpdate();
-        });
+    if (updates.isEnabled) {
+      interval(10 * 60 * 60 * 1000).subscribe(() => {
+        updates.checkForUpdate();
+      });
 
       updates.available.subscribe(event => {
         const snackBarRef = this.snackBar.open(
