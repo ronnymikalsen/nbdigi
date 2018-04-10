6c27a76676d7400298de725bbc7c79337929725c
ronnymikalsen <ronny.mikalsen@nb.no>
2018-04-10 10:18:38 +0200

    feat: item activity

--- src/app/item-list/containers/default-item-list/default-item-list.component.ts
+++ src/app/item-list/containers/default-item-list/default-item-list.component.ts
@@ -12,12 +12,12 @@ import * as fromRoot from './../../../+state/reducers';
 import { Item } from './../../../models/search-result.model';
 
 @Component({
-  selector: 'app-default-item-card',
-  templateUrl: './default-item-card.component.html',
-  styleUrls: ['./default-item-card.component.scss'],
+  selector: 'app-default-item-list',
+  templateUrl: './default-item-list.component.html',
+  styleUrls: ['./default-item-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DefaultItemCardComponent implements OnInit {
+export class DefaultItemListComponent implements OnInit {
   @Input() item: Item;
   isDebugOn: Observable<boolean> = this.store.select(fromRoot.isDebugOn);
 
--- src/app/my-activity/my-activity.module.ts
+++ src/app/my-activity/my-activity.module.ts
@@ -1,20 +1,22 @@
-import { CriteriaListItemModule } from './../criteria-list-item/criteria-list-item.module';
-import { ItemActivityComponent } from './components/item-activity/item-activity.component';
-import { SearchActivityComponent } from './components/search-activity/search-activity.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
-import { MyActivityRoutingModule } from './my-activity-routing.module';
-import { MyActivityPageComponent } from './containers/my-activity-page';
+import { CriteriaListItemModule } from './../criteria-list-item/criteria-list-item.module';
+import { ItemListModule } from './../item-list/item-list.module';
+import { ItemActivityComponent } from './components/item-activity/item-activity.component';
 import { MyActivityComponent } from './components/my-activity/my-activity.component';
+import { SearchActivityComponent } from './components/search-activity/search-activity.component';
+import { MyActivityPageComponent } from './containers/my-activity-page';
+import { MyActivityRoutingModule } from './my-activity-routing.module';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule,
     MyActivityRoutingModule,
-    CriteriaListItemModule
+    CriteriaListItemModule,
+    ItemListModule
   ],
   declarations: [
     MyActivityPageComponent,
