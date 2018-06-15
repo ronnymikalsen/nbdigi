b3e3ee145739982cb44d5c19be47b7a31f07ddd7
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2018-06-15 10:38:41 +0200

    refactor:

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,33 +3,33 @@ Object.defineProperty(exports, '__esModule', { value: true });
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 admin.initializeApp(functions.config().firebase);
-exports.updateFavorite3 = functions.firestore
+exports.updateFavorite = functions.firestore
   .document('users/{userId}/items/{messageId}')
   .onWrite((snapshot, context) => {
-    const data = snapshot.after;
-    const currentCanvasId = data.get('currentCanvasId');
+    const updatedItem = snapshot.after;
+    const currentCanvasId = updatedItem.get('currentCanvasId');
     return snapshot.after.ref.parent.parent
       .collection('favorites')
       .get()
-      .then(collections => {
-        collections.forEach(snap => {
-          snap.ref
+      .then(favorites => {
+        favorites.forEach(favorite => {
+          favorite.ref
             .collection('items')
             .get()
-            .then(i => {
-              i.forEach(hmm => {
-                if (hmm.id === data.id) {
-                  hmm.ref
+            .then(items => {
+              items.forEach(item => {
+                if (item.id === updatedItem.id) {
+                  item.ref
                     .update({
                       currentCanvasId: currentCanvasId
                     })
-                    .catch(err => console.log(err));
+                    .catch(err => console.error(err));
                 }
               });
             })
-            .catch(err => console.log(err));
+            .catch(err => console.error(err));
         });
       })
-      .catch(err => console.log(err));
+      .catch(err => console.error(err));
   });
 //# sourceMappingURL=index.js.map
