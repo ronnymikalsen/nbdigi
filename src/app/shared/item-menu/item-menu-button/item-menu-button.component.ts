fcc6c528b86becb3ac89fee20f8ad09332ea8e79
Ronny Mikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2018-06-18 23:12:22 +0200

    feat: open item from menu

--- src/app/shared/item-menu/item-menu-button/item-menu-button.component.ts
+++ src/app/shared/item-menu/item-menu-button/item-menu-button.component.ts
@@ -6,11 +6,11 @@ import {
 } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Store } from '@ngrx/store';
-import { FavoriteList } from '../../models/favorite-list';
-import { Item } from '../../models/search-result.model';
-import * as favoriteActions from './../../+state/actions/favorite.actions';
-import * as fromRoot from './../../+state/reducers';
-import * as itemActions from './../../+state/actions/item.actions';
+import { FavoriteList } from '../../../models/favorite-list';
+import { Item } from '../../../models/search-result.model';
+import * as favoriteActions from './../../../+state/actions/favorite.actions';
+import * as itemActions from './../../../+state/actions/item.actions';
+import * as fromRoot from './../../../+state/reducers';
 
 @Component({
   selector: 'app-item-menu-button',
@@ -21,7 +21,6 @@ import * as itemActions from './../../+state/actions/item.actions';
 export class ItemMenuButtonComponent implements OnInit {
   @Input() item: Item;
   @Input() list: FavoriteList;
-  @Input() config = new ItemMenuButtonComponentConfig();
 
   constructor(private store: Store<fromRoot.State>, public dialog: MatDialog) {}
 
@@ -42,16 +41,5 @@ export class ItemMenuButtonComponent implements OnInit {
         items: [this.item]
       })
     );
-  }
-}
-
-export class ItemMenuButtonComponentConfig {
-  enableOpen = true;
-
-  constructor(fields?: { enableOpen?: boolean }) {
-    if (fields) {
-      this.enableOpen =
-        fields.enableOpen !== undefined ? fields.enableOpen : this.enableOpen;
-    }
   }
 }
--- src/app/viewer/viewer.module.ts
+++ src/app/viewer/viewer.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { AddToFavoriteListDialogComponent } from '../my-library/containers/add-to-favorite-list-dialog/add-to-favorite-list-dialog.component';
+import { ItemMenuModule } from '../item-menu/item-menu.module';
 import { MyLibraryModule } from '../my-library/my-library.module';
 import { SharedModule } from '../shared/shared.module';
 import { ViewerComponent } from './components/viewer/viewer.component';
 import { ViewerPageComponent } from './containers/viewer-page';
-import { ItemMenuModule } from '../item-menu/item-menu.module';
 
 @NgModule({
   imports: [CommonModule, SharedModule, MyLibraryModule, ItemMenuModule],
