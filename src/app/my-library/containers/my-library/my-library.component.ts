31f5ea5e2264d995edb8a97e96359ef98b4183a2
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2018-06-21 13:08:55 +0200

    feat: recent activity

--- src/app/my-library/containers/my-library/my-library.component.ts
+++ src/app/my-library/containers/my-library/my-library.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-
+import { map } from 'rxjs/operators';
+import { Item, MediaTypeResults } from '../../../models/search-result.model';
 import * as favoriteActions from './../../../+state/actions/favorite.actions';
 import * as fromRoot from './../../../+state/reducers';
 import { FavoriteList } from './../../../models/favorite-list';
@@ -12,9 +13,26 @@ import { FavoriteList } from './../../../models/favorite-list';
   styleUrls: ['./my-library.component.scss']
 })
 export class MyLibraryComponent implements OnInit {
+  isDebugOn: Observable<boolean> = this.store.select(fromRoot.isDebugOn);
   favoriteLists: Observable<FavoriteList[]> = this.store.select(
     fromRoot.getFavoriteList
   );
+  recentActivity: Observable<MediaTypeResults> = this.store
+    .select(fromRoot.getFavoriteList)
+    .pipe(
+      map((favoriteLists: FavoriteList[]) => {
+        let items = [];
+        favoriteLists.forEach(l => {
+          items = [...items, ...l.items];
+        });
+        items.sort(
+          (a, b) =>
+            (b.timestamp ? b.timestamp.toMillis() : 0) -
+            (a.timestamp ? a.timestamp.toMillis() : 0)
+        );
+        return new MediaTypeResults({ items: items });
+      })
+    );
 
   constructor(private store: Store<fromRoot.State>) {}
 
