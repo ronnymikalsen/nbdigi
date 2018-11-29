4d120ab358648075ea88a3ae6d41cb305b3289b3
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2018-11-29 13:33:26 +0100

    wip

--- libs/authentication-state/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/+state/authenticationstate.facade';
-export * from './lib/+state/authenticationstate.reducer';
-export * from './lib/+state/authenticationstate.selectors';
-export * from './lib/authentication-state.module';
