97e82e58f1fec714e696c6b7cd5d255620bb20ec
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-11-10 12:16:16 +0100

    wip:

--- apps/nbdigi/jest.config.js
+++ apps/nbdigi/jest.config.js
@@ -2,15 +2,6 @@ module.exports = {
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/apps/nbdigi/',
   setupFilesAfterEnv: ['<rootDir>src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
-      tsconfig: '<rootDir>tsconfig.spec.json',
-    },
-  },
+  globals: {},
   displayName: 'nbdigi',
 };
